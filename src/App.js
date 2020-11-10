d5f8f0d29636765528747da0375984d48a392e0d
Ruslan-Almukhanov <ruslanalmukhanov@gmail.com>
2020-11-10 11:17:12 +0500

    data was added to the table

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import ClientAccounts from './components/ClientAccounts'
 
--- src/components/ClientAccounts.js
+++ src/components/ClientAccounts.js
@@ -9,55 +9,6 @@ import TableHead from "@material-ui/core/TableHead";
 import TablePagination from "@material-ui/core/TablePagination";
 import TableRow from "@material-ui/core/TableRow";
 
-const columns = [
-  { id: "name", label: "Name", minWidth: 170 },
-  { id: "code", label: "ISO\u00a0Code", minWidth: 100 },
-  {
-    id: "population",
-    label: "Population",
-    minWidth: 170,
-    align: "right",
-    format: value => value.toLocaleString("en-US")
-  },
-  {
-    id: "size",
-    label: "Size\u00a0(km\u00b2)",
-    minWidth: 170,
-    align: "right",
-    format: value => value.toLocaleString("en-US")
-  },
-  {
-    id: "density",
-    label: "Density",
-    minWidth: 170,
-    align: "right",
-    format: value => value.toFixed(2)
-  }
-];
-
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
-
-const rows = [
-  createData("India", "IN", 1324171354, 3287263),
-  createData("China", "CN", 1403500365, 9596961),
-  createData("Italy", "IT", 60483973, 301340),
-  createData("United States", "US", 327167434, 9833520),
-  createData("Canada", "CA", 37602103, 9984670),
-  createData("Australia", "AU", 25475400, 7692024),
-  createData("Germany", "DE", 83019200, 357578),
-  createData("Ireland", "IE", 4857000, 70273),
-  createData("Mexico", "MX", 126577691, 1972550),
-  createData("Japan", "JP", 126317000, 377973),
-  createData("France", "FR", 67022000, 640679),
-  createData("United Kingdom", "GB", 67545757, 242495),
-  createData("Russia", "RU", 146793744, 17098246),
-  createData("Nigeria", "NG", 200962417, 923768),
-  createData("Brazil", "BR", 210147125, 8515767)
-];
-
 const useStyles = makeStyles({
   root: {
     width: "100%"
@@ -69,17 +20,6 @@ const useStyles = makeStyles({
 
 const ClientAccounts = () => {
   const classes = useStyles();
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = event => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
 
   const data = [
     {
@@ -112,48 +52,22 @@ const ClientAccounts = () => {
     <Paper className={classes.root}>
       <TableContainer className={classes.container}>
         <Table
-          stye={{ width: "1000px" }}
-          stickyHeader
+          stye={{ background: "#ccc" }}
           aria-label="sticky table"
         >
-          <TableHead>
-            <TableRow>
-              {columns.map(column => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
           <TableBody>
-            
-                return (
-                  <TableRow>
-                    {data.map((client, index) => {
-                      return (
-                        <TableCell key={index}>
-                          {client.accountNumber}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-               
+            {data.map((client, index) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell>{client.accountNumber}</TableCell>
+                  <TableCell>{client.sum}</TableCell>
+                  <TableCell>{client.currency}</TableCell>
+                </TableRow>
+              );
+            })}
           </TableBody>
         </Table>
       </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onChangePage={handleChangePage}
-        onChangeRowsPerPage={handleChangeRowsPerPage}
-      />
     </Paper>
   );
 };
